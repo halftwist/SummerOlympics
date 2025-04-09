9b78d47e75c423497d78ca0ba176f0e20c6698c1
John Kearon <jkearon@hotmail.com>
2025-04-09 14:35:32 -0400

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by John Kearon on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
